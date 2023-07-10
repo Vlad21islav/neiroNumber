98e0d419ac000ba61ecef43f257d0fb6263bfba8
Vlad21Islav <vmitk@mail.ru>
2023-07-10 12:49:26 +0300

    my first commit

---
+++ test.py
@@ -0,0 +1,116 @@
+import pygame
+from numpy import *
+from math import sqrt as s
+
+
+inp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+     0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+     1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+pygame.init()
+
+screen = pygame.display.set_mode((200, 200), flags=pygame.RESIZABLE)
+pygame.display.set_caption('Mars protection')
+width, height = pygame.display.get_surface().get_size()
+
+sqr_size = int(height / 29.6)
+sqrs = 28 * 28
+
+path = ''
+
+square = (
+        [pygame.image.load(path + 'images/images/square.png')] * sqrs
+)
+
+square_white = pygame.transform.scale(pygame.image.load(path + 'images/images/square.png').convert_alpha(),
+                                      (sqr_size, sqr_size))
+square_black = pygame.transform.scale(pygame.image.load(path + 'images/images/square_black.png').convert_alpha(),
+                                      (sqr_size, sqr_size))
+
+square_rect = []
+
+list_sqr = inp
+righting = False
+
+running = True
+while running:
+
+    screen.fill('gray')
+
+    if height > 500:
+        screen = pygame.display.set_mode((width, 500), flags=pygame.RESIZABLE)
+    square_rect = []
+    width, height = pygame.display.get_surface().get_size()
+    sqr_size = int(height / 29.6)
+
+    square_white = pygame.transform.scale(pygame.image.load(path + 'images/images/square.png').convert_alpha(),
+                                          (sqr_size, sqr_size))
+    square_black = pygame.transform.scale(pygame.image.load(path + 'images/images/square_black.png').convert_alpha(),
+                                          (sqr_size, sqr_size))
+
+    mouse = pygame.mouse.get_pos()
+    keys = pygame.key.get_pressed()
+
+    for i in range(len(square)):
+        if inp[i] == 1:
+            square[i] = square_black
+        else:
+            square[i] = square_white
+
+    x = (width - (sqr_size * 28)) / 2 - sqr_size
+    y = sqr_size
+    for quantity in range(len(inp) + 1):
+        for high in range(int(s(len(inp)))):
+            if quantity == high * 28 + 1 and high != 0:
+                y += sqr_size
+                x = (width - (sqr_size * 28)) / 2
+        if quantity != 0:
+            screen.blit(pygame.transform.scale(square[quantity - 1].convert_alpha(), (sqr_size, sqr_size)), (x, y))
+            square_rect.append(square[quantity - 1].get_rect(topleft=(x, y)))
+        x += sqr_size
+
+    if righting:
+        for squares in range(len(square)):
+            mouse = pygame.mouse.get_pos()
+            keys = pygame.key.get_pressed()
+            if square_rect[squares].collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+                square[squares] = square_black
+                list_sqr[squares] = 1
+    else:
+        for squares in range(len(square)):
+            mouse = pygame.mouse.get_pos()
+            keys = pygame.key.get_pressed()
+            if square_rect[squares].collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+                square[squares] = square_white
+                list_sqr[squares] = 0
+
+    pygame.display.update()
+
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if keys[pygame.K_r]:
+            righting = True
+        if keys[pygame.K_l]:
+            righting = False
+        if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+            running = False
+            print(list_sqr)
+            pygame.quit()
